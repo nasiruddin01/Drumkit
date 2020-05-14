02612134924781b28333f6aec97b703670fd311f
nasiruddin01 <nasiruddin.dev@gmail.com>
2020-05-15 00:24:03 +0600

    3rd commit  javascript

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
-    })
+        console.log(this.style.color = "white")
+    });
 }
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();//
